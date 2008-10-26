dc83fc66b5af490ba382814e4ca03a7485e56d9a
christoffer.lerno <christoffer.lerno@866981b8-a2bd-11dd-91bf-632da8b6e10b>
2008-10-26 08:18:56 +0000

    Added possibility to use DTD-validation.

--- src/main/xmlwise/Xmlwise.java
+++ src/main/xmlwise/Xmlwise.java
@@ -7,9 +7,11 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
 import java.io.IOException;
 import java.io.StringReader;
+import java.io.File;
 
 /**
- * XML helper methods.
+ * Xmlwise convenience methods for loading xml documents and render them into
+ * XmlElement trees.
  *
  * @author Christoffer Lerno
  */
@@ -18,19 +20,66 @@ public class Xmlwise
 	private Xmlwise() {}
 
 	/**
-	 * Creates a DOM Document from the specified XML string, ignoring DTD-validation.
+	 * Loads an XML document ignoring DTD-validation.
 	 *
-	 * @param xml a valid XML document, ie the String can't be null or empty
-	 * @return the <code>Document</code> object for the specified string.
-	 * @throws XmlParseException if we fail to parse the XML.
+	 * @param file the file to read from.
+	 * @return an XML document.
+	 * @throws IOException if we fail to load the file.
+	 * @throws XmlParseException if there is a problem parsing the xml in the file.
 	 */
-	public static Document createDocument(String xml) throws XmlParseException
+	public static Document loadDocument(File file) throws IOException, XmlParseException
+	{
+		return loadDocument(file, false, false);
+	}
+
+	/**
+	 * Loads an XML document.
+	 *
+	 * @param file the file to read from.
+	 * @param validate if we should validate the document or not.
+	 * @param loadExternalDTD true to allow loading of external dtds.
+	 * @return an XML document.
+	 * @throws IOException if we fail to load the file.
+	 * @throws XmlParseException if there is a problem parsing the xml in the file.
+	 */
+	public static Document loadDocument(File file, boolean validate, boolean loadExternalDTD) throws IOException, XmlParseException
 	{
 		try
 		{
 			DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-			documentBuilderFactory.setAttribute("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
-			documentBuilderFactory.setValidating(false);
+			documentBuilderFactory.setAttribute("http://apache.org/xml/features/nonvalidating/load-external-dtd",
+			                                    loadExternalDTD);
+			documentBuilderFactory.setValidating(validate);
+			DocumentBuilder builder = documentBuilderFactory.newDocumentBuilder();
+			return builder.parse(file);
+		}
+		catch (IOException e)
+		{
+			throw e;
+		}
+		catch (Exception e)
+		{
+			throw new XmlParseException(e);
+		}
+	}
+
+	/**
+	 * Creates a DOM Document from the specified XML string, ignoring DTD-validation.
+	 *
+	 * @param xml a valid XML document, ie the String can't be null or empty
+	 * @param validate if we should validate the document or not.
+	 * @param loadExternalDTD true to allow loading of external dtds.
+	 * @return the <code>Document</code> object for the specified string.
+	 * @throws XmlParseException if we fail to parse the XML.
+	 */
+	public static Document createDocument(String xml, boolean validate, boolean loadExternalDTD) throws XmlParseException
+	{
+		try
+		{
+			DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+			documentBuilderFactory.setAttribute("http://apache.org/xml/features/nonvalidating/load-external-dtd",
+			                                    loadExternalDTD);
+			documentBuilderFactory.setValidating(validate);
 			DocumentBuilder builder = documentBuilderFactory.newDocumentBuilder();
 			return builder.parse(new InputSource(new StringReader(xml)));
 		}
@@ -41,31 +90,15 @@ public class Xmlwise
 	}
 
 	/**
-	 * Loads an XML document ignoring DTD-validation.
+	 * Creates a DOM Document from the specified XML string, ignoring DTD-validation.
 	 *
-	 * @param fileName the path to the file.
-	 * @return an XML document.
-	 * @throws IOException if we fail to load the file.
-	 * @throws XmlParseException if there is a problem parsing the xml in the file.
+	 * @param xml a valid XML document, ie the String can't be null or empty
+	 * @return the <code>Document</code> object for the specified string.
+	 * @throws XmlParseException if we fail to parse the XML.
 	 */
-	public static Document loadDocument(String fileName) throws IOException, XmlParseException
+	public static Document createDocument(String xml) throws XmlParseException
 	{
-		try
-		{
-			DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-			documentBuilderFactory.setAttribute("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
-			documentBuilderFactory.setValidating(false);
-			DocumentBuilder builder = documentBuilderFactory.newDocumentBuilder();
-			return builder.parse(fileName);
-		}
-		catch (IOException e)
-		{
-			throw e;
-		}
-		catch (Exception e)
-		{
-			throw new XmlParseException(e);
-		}
+		return createDocument(xml, false, false);
 	}
 
 	/**
@@ -118,21 +151,34 @@ public class Xmlwise
 	/**
 	 * Loads a document from file and transforms it into an XmlElement tree.
 	 *
+	 * @param file the file to load.
+	 * @return an XmlElement tree rendered from the file.
+	 * @throws XmlParseException if parsing the file failed for some reason.
+	 * @throws IOException if there were any problems reading from the file.
+	 */
+	public static XmlElement loadXml(File file) throws XmlParseException, IOException
+	{
+		return new XmlElement(loadDocument(file).getDocumentElement());
+	}
+
+	/**
+	 * Loads a document from file and transforms it into an XmlElement tree.
+	 *
 	 * @param filename the path to the file.
-	 * @return an XmlElement to use.
+	 * @return an XmlElement tree rendered from the file.
 	 * @throws XmlParseException if parsing the file failed for some reason.
 	 * @throws IOException if there were any problems reading from the file.
 	 */
 	public static XmlElement loadXml(String filename) throws XmlParseException, IOException
 	{
-		return new XmlElement(loadDocument(filename).getDocumentElement());
+		return loadXml(new File(filename));
 	}
 
 	/**
 	 * Creates a document from a string and transforms it into an XmlElement tree.
 	 *
 	 * @param xml the xml as a string.
-	 * @return an XmlElement to use.
+	 * @return an XmlElement tree rendered from the file.
 	 * @throws XmlParseException if parsing the xml failed to validate for some reason.
 	 */
 	public static XmlElement createXml(String xml) throws XmlParseException
